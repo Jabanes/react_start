dd351f4509804f2804a352a9e09d07480c591a74
Jabanes <erezhabani2003@gmail.com>
2025-02-12 14:58:16 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='lion.png' className="App-logo" alt="logo" />
+        <p>
+        "Searching for a lion in the desert is like seeking truth within ourselves—rare, elusive, and hidden beneath the surface, we must look beyond the barren landscape of doubt, for only with patience and clarity can we find the strength that lies quietly waiting."
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Find the lion
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
